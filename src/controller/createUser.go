9ce39af21f9c5c301f3474e1136972e886eac342
Joao <jvmarkes@gmail.com>
2024-06-10 18:18:23 +0100

    creating user object

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,12 +1,24 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joaaomarques/crud-golang/src/configuration/rest_err"
+	"github.com/joaaomarques/crud-golang/src/model/request"
 )
 
 func CreateUser(c *gin.Context) {
 
-	err := rest_err.NewBadRequestError("Wrong route called!")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
